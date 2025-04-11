2d1d52c340a5aa0e20bafc5744107b2027f752cc
kamzhanyue <kamzhanyue@gmail.com>
2025-04-12 00:51:39 +1000

    feat: add shapes in r3f

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,149 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Canvas,
+  Color,
+  ThreeEvent,
+  useFrame,
+  Vector3,
+} from "@react-three/fiber";
+import "./App.css";
+import { useRef, useState } from "react";
+import { Mesh } from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface CubeProps {
+  position: Vector3;
+  size?: [number, number, number];
+  colour: Color;
 }
 
-export default App
+const Cube = ({ position, size, colour }: CubeProps) => {
+  const ref = useRef<Mesh>(null!);
+
+  useFrame((_state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 2.0;
+  });
+
+  return (
+    <mesh ref={ref} position={position}>
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={colour} />
+    </mesh>
+  );
+};
+
+interface SphereProps {
+  position: Vector3;
+  radius: number;
+  heightSegments: number;
+  widthSegments: number;
+  colour: Color;
+}
+
+const Sphere = ({
+  position,
+  radius,
+  heightSegments,
+  widthSegments,
+  colour,
+}: SphereProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  const ref = useRef<Mesh>(null!);
+
+  useFrame((_state, delta) => {
+    const speed = isHovered ? 5 : 0.2;
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * speed;
+  });
+
+  const handlePointerEnter = (event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation();
+    setIsHovered(true);
+  };
+
+  const handlePointerLeave = (event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation();
+    setIsHovered(false);
+  };
+
+  const handlePointerDown = (event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation();
+    setIsClicked((prev) => !prev);
+  };
+
+  return (
+    <mesh
+      ref={ref}
+      position={position}
+      onPointerEnter={handlePointerEnter}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      scale={isClicked ? 3 : 1}
+    >
+      <sphereGeometry args={[radius, heightSegments, widthSegments]} />
+      <meshStandardMaterial color={colour} wireframe />
+    </mesh>
+  );
+};
+
+interface TorusProps {
+  position: Vector3;
+  radius: number;
+  tube: number;
+  radialSegments: number;
+  tubularSegments: number;
+  arc: number;
+  colour: Color;
+}
+
+const Torus = ({
+  position,
+  radius,
+  tube,
+  radialSegments,
+  tubularSegments,
+  arc,
+  colour,
+}: TorusProps) => {
+  const ref = useRef<Mesh>(null!);
+  return (
+    <mesh ref={ref} position={position}>
+      <torusGeometry
+        args={[radius, tube, radialSegments, tubularSegments, arc]}
+      />
+      <meshStandardMaterial color={colour} />
+    </mesh>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Canvas>
+        <ambientLight />
+        <directionalLight position={[0, 0, 2]} />
+        <group position={[0, 0, 0]}>
+          <Cube position={[2, 0, 0]} size={[2, 2, 2]} colour="orange" />
+          <Sphere
+            position={[-2, 0, 0]}
+            radius={1}
+            widthSegments={30}
+            heightSegments={30}
+            colour="pink"
+          />
+          <Torus
+            position={[0, 0, 0]}
+            radius={0.5}
+            tube={0.1}
+            radialSegments={30}
+            tubularSegments={30}
+            arc={3.14}
+            colour="blue"
+          />
+        </group>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
